613a7fa5f8d456615b094dc88635f3d19a32774a
Umar Solanki <uysolanki@gmail.com>
2024-09-20 17:57:54 +0530

    day8

---
+++ src/day8/NestedSwitchDemo1.java
@@ -0,0 +1,58 @@
+package day8;
+
+import java.util.Scanner;
+
+public class NestedSwitchDemo1 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int choice;
+		System.out.println("1. English");
+		System.out.println("2. Arabic");
+		System.out.println("0. Exit");
+		System.out.println("Enter Choice : ");
+		choice=sc.nextInt();
+		
+		switch (choice) {
+		case 1:
+			System.out.println("1. UK");
+			System.out.println("2. US");
+			System.out.println("3. AUS");
+			System.out.println("0. Exit");
+			System.out.println("Enter Choice : ");
+			choice=sc.nextInt();
+			switch(choice)
+			{
+			case 1 : System.out.println("Call Routed to London");break;
+			case 2 : System.out.println("Call Routed to New York");break;
+			case 3 : System.out.println("Call Routed to Sydney");break;
+			case 0 :System.exit(0);
+			default : System.out.println("Invakid Input");break;
+			}
+			
+		break;
+		case 2:
+			System.out.println("1. Qatar");
+			System.out.println("2. Oman");
+			System.out.println("3. Dubai");
+			System.out.println("0. Exit");
+			System.out.println("Enter Choice : ");
+			choice=sc.nextInt();
+			switch(choice)
+			{
+			case 1 : System.out.println("Call Routed to Doha");break;
+			case 2 : System.out.println("Call Routed to Muscat");break;
+			case 3 : System.out.println("Call Routed to Abu Dhabi");break;
+			case 0 :System.exit(0);
+			default : System.out.println("Invakid Input");break;
+			}
+		break;
+		case 0:
+			System.exit(0);
+		break;
+		default:System.out.println("Invalid input");
+			break;
+		}
+	}
+
+}
