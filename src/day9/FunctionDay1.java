99dc7ba79acb4c58b4a01ad848a8fdad6e855877
Umar Solanki <uysolanki@gmail.com>
2024-09-23 18:53:12 +0530

    day 9

---
+++ src/day9/FunctionDay1.java
@@ -0,0 +1,23 @@
+package day9;
+
+public class FunctionDay1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hi");								//1
+		sharikShaikh();    //main in sleep mode
+		System.out.println("Bye");								//4
+	}
+	
+	public static void sharikShaikh()
+	{
+		System.out.println("ITV");								//2
+		sumitSomase();
+		
+	}
+
+	public static void sumitSomase()
+	{
+		System.out.println("Ferguson College");					//3
+	}
+	
+}
